32b1c141337b38482587e6be4f0ff9d8160a2c6b
Yisrael Bar <yisrael35@gmail.com>
2021-09-21 18:51:19 +0300

    .

--- src/pages/ForgotPassword.js
+++ src/pages/ForgotPassword.js
@@ -3,7 +3,6 @@ import { Redirect } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 
 import * as authActions from '../redux/Auth/action'
-import axios from 'axios'
 const words_he = require('../utils/words_he').words_he
 
 const ForgotPassword = (props) => {
@@ -14,10 +13,7 @@ const ForgotPassword = (props) => {
   const submit = async (e) => {
     e.preventDefault()
     dispatch(authActions.forgotPassword(username))
-    // try {
-      // await axios.post(process.env.REACT_APP_REST_IMJ_URL + '/forgot_password', { username })
       setRedirect(true)
-    // } catch (error) {}
   }
 
   if (redirect) {
@@ -33,9 +29,6 @@ const ForgotPassword = (props) => {
       <form onSubmit={submit}>
         <h1 className='h3 mb-3 fw-normal'>{words_he['forgot_password']}</h1>
         <input type='text' className='form-control' placeholder={words_he['username']} required onChange={(e) => setUsername(e.target.value)} />
-
-        {/* <input type='password' className='form-control' placeholder={words_he['username']} required onChange={(e) => setPassword(e.target.value)} /> */}
-
         <button className='w-100 btn btn-lg btn-primary' type='submit'>
           {words_he['send']}
         </button>

67aaa367aab8bbb0a5523778a5af5ddd87a17d0f
Yisrael Bar <yisrael35@gmail.com>
2022-01-06 22:29:01 +0200

    update in big Calendar packege

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,25 @@
 import React from 'react'
-import BigCalendar from 'react-big-calendar-like-google'
+import { Calendar, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment'
-import 'react-big-calendar-like-google/lib/css/react-big-calendar.css'
-BigCalendar.momentLocalizer(moment)
-const words_he = require('../utils/words_he').words_he
-let allViews = Object.keys(BigCalendar.Views).map((k) => BigCalendar.Views[k])
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import 'moment/locale/he'
+// moment().locale('he')
+
+const localizer = momentLocalizer(moment)
+// const words_he = require('../utils/words_he').words_he
 const Home = (props) => {
   return (
     <div style={{ textAlign: 'center' }}>
       {/* <a href='http://localhost:3001/assets/README.md'>readme</a> */}
-
-      <p>{words_he['welcome']}</p>
-      {/* {props.name ? `${words_he['hello']} ` + props.name : `${words_he['please_sign_in']}`} */}
-      <BigCalendar events={myEventsList}  startAccessor='startDate' endAccessor='endDate' />
+      {/* <p>{words_he['welcome']}</p> */}
+      <Calendar
+        localizer={localizer}
+        events={myEventsList}
+        // views={['month']}
+        startAccessor='start'
+        endAccessor='end'
+        style={{ height: 500 }}
+      />
     </div>
   )
 }
@@ -20,111 +27,68 @@ const Home = (props) => {
 const myEventsList = [
   {
     title: 'All Day Event very long title',
-    bgColor: '#ff7f50',
+    color: '#ff7f50',
     allDay: true,
-    start: new Date(2021, 12, 0),
-    end: new Date(2021, 12, 1),
+    start: '2022-01-01',
+    end: '2022-01-01',
   },
   {
     title: 'Long Event',
-    start: new Date(2021, 12, 7),
-    end: new Date(2021, 12, 10),
+    start: '2022-01-06',
+    end: '2022-01-06',
   },
-
   {
     title: 'DTS STARTS',
     bgColor: '#dc143c',
-    start: new Date(2016, 2, 13, 0, 0, 0),
-    end: new Date(2016, 2, 20, 0, 0, 0),
+    start: '2022-01-08',
+    end: '2022-01-10',
   },
 
   {
     title: 'DTS ENDS',
     bgColor: '#ff8c00',
-    start: new Date(2016, 10, 6, 0, 0, 0),
-    end: new Date(2016, 10, 13, 0, 0, 0),
+    start: '2022-02-16',
+    end: '2022-02-16',
   },
 
   {
     title: 'Some Event',
     bgColor: '#9932cc',
-    start: new Date(2021, 12, 9, 0, 0, 0),
-    end: new Date(2021, 12, 9, 0, 0, 0),
+    start: '2022-02-26',
+    end: '2022-02-26',
   },
   {
     title: 'Conference',
     bgColor: '#e9967a',
-    start: new Date(2021, 12, 11),
-    end: new Date(2021, 12, 13),
+    start: '2022-02-26',
+    end: '2022-02-26',
     desc: 'Big conference for important people',
   },
   {
     title: 'Meeting',
     bgColor: '#8fbc8f',
-    start: new Date(2021, 12, 12, 10, 30, 0, 0),
-    end: new Date(2021, 12, 12, 12, 30, 0, 0),
+    start: '2022-03-26',
+    end: '2022-03-26',
     desc: 'Pre-meeting meeting, to prepare for the meeting',
   },
   {
     title: 'Lunch',
     bgColor: '#cd5c5c',
-    start: new Date(2021, 12, 12, 12, 0, 0, 0),
-    end: new Date(2021, 12, 12, 13, 0, 0, 0),
+    start: '2022-01-26',
+    end: '2022-01-26',
     desc: 'Power lunch',
   },
   {
     title: 'Happy Hour',
-    start: new Date(2021, 12, 12, 12, 0, 0, 0),
-    end: new Date(2021, 12, 12, 13, 0, 0, 0),
+    start: '2022-02-13',
+    end: '2022-02-13',
     desc: 'Power lunch happy hour',
   },
   {
     title: 'Meeting',
     bgColor: '#da70d6',
-    start: new Date(2021, 12, 12, 14, 0, 0, 0),
-    end: new Date(2021, 12, 12, 15, 0, 0, 0),
-  },
-  {
-    title: 'Happy Hour',
-    bgColor: '#eee8aa',
-    start: new Date(2021, 12, 17, 17, 0, 0, 0),
-    end: new Date(2021, 12, 17, 17, 30, 0, 0),
-    desc: 'Most important meal of the day',
-  },
-  {
-    title: 'Dinner',
-    bgColor: '#98fb98',
-    start: new Date(2021, 12, 15, 20, 0, 0, 0),
-    end: new Date(2021, 12, 15, 21, 0, 0, 0),
-  },
-  {
-    title: 'Birthday Party',
-    bgColor: '#afeeee',
-    start: new Date(2021, 12, 13, 7, 0, 0),
-    end: new Date(2021, 12, 13, 10, 30, 0),
-  },
-  {
-    title: 'Birthday Party 2',
-    bgColor: '#db7093',
-    start: new Date(2021, 12, 13, 7, 0, 0),
-    end: new Date(2021, 12, 13, 10, 30, 0),
-  },
-  {
-    title: 'Birthday Party 3',
-    bgColor: '#cd853f',
-    start: new Date(2021, 12, 13, 7, 0, 0),
-    end: new Date(2021, 12, 13, 10, 30, 0),
-  },
-  {
-    title: 'Late Night Event',
-    bgColor: '#b0e0e6',
-    start: new Date(2021, 12, 17, 19, 30, 0),
-    end: new Date(2021, 12, 18, 2, 0, 0),
-  },
-  {
-    title: 'Multi-day Event',
-    start: new Date(2021, 12, 20, 19, 30, 0),
-    end: new Date(2021, 12, 22, 2, 0, 0),
+    start: '2022-02-12',
+    end: '2022-02-12',
   },
 ]
 

6a3b65f8d95466d755f4871e40bf7b1e7255827a
eladdavid5 <elad.david5@gmail.com>
2022-03-09 02:58:21 +0200

    grid client

--- src/components/pages/UpdateClient.js
+++ src/components/pages/UpdateClient.js
@@ -3,9 +3,14 @@ import '../../css/clients.css'
 import { useDispatch } from 'react-redux'
 import * as action_client from '../../redux/Client/action'
 import * as action_popUp from '../../redux/PopUp/action'
-import Select from 'react-select'
+import Select from '@material-ui/core/Select'
 import * as actionSnackBar from '../../redux/SnackBar/action'
-
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
+import { Container } from '@material-ui/core'
+import Typography from '@material-ui/core/Typography'
+import InputBase from '@material-ui/core/InputBase'
+import MenuItem from '@material-ui/core/MenuItem'
 const { invalid_email, invalid_phone, all_fields_filled, invalid_email_characters } = require('../../utils/validate_helper')
 const words_he = require('../../utils/words_he').words_he
 
@@ -98,146 +103,113 @@ const UpdateClient = (props) => {
 
   return (
     <div>
-      {/* <form className='form-signin'>
-        <label>
-          {words_he['name'] + '  '}
-          <input type='text' onChange={(e) => setClientInfo({ ...client_info, name: e.target.value })} defaultValue={client_info.name} />
-        </label>
-        <label>
-          <div className='designed-div'>
-            {words_he['type']}
-            {/* <select onChange={(e) => setClientInfo({ ...client_info, type: e.value })}>
-            <option value='grapefruit'>Grapefruit</option>
-            <option value='lime'>Lime</option>
-            <option selected value='coconut'>
-              Coconut
-            </option>
-            <option value='mango'>Mango</option>
-          </select> */}
-      {/* <Select className={'designed-select'} placeholder={words_he['type']} options={types} label={words_he['type']} onChange={(e) => setClientInfo({ ...client_info, type: e.value })} />
-          </div> */}
-      {/* <ul>
-            <li>
-              <input
-                type='radio'
-                value={words_he['private']}
-                name={'type'}
-                checked={client_info.type === words_he['private']}
-                onChange={(e) => setClientInfo({ ...client_info, type: e.target.value })}
+      <Grid container>
+        <Grid item xs={12}>
+          <Grid container justifyContent='center'>
+            {' '}
+            <Grid item xs={1}>
+              {' '}
+              <Typography>
+                <b>{words_he['name']}</b>
+              </Typography>{' '}
+            </Grid>{' '}
+            <Grid item xs={1}>
+              {' '}
+              <InputBase
+                type='text'
+                onChange={(e) => setClientInfo({ ...client_info, name: e.target.value })}
+                defaultValue={client_info.name}
+                style={{ borderStyle: 'solid', borderWidth: 1, borderRadius: 5, width: 180 }}
               />{' '}
-              {words_he['private']}
-            </li>
-            <li>
-              <input
-                type='radio'
-                value={words_he['company']}
-                name={'type'}
-                checked={client_info.type === words_he['company']}
-                onChange={(e) => console.log({ ...client_info, type: e.target.value })}
+            </Grid>{' '}
+          </Grid>
+        </Grid>
+
+        <Grid item xs={12}>
+          <Grid container justifyContent='center'>
+            {' '}
+            <Grid item xs={1}>
+              {' '}
+              <Typography>
+                <b>{words_he['type']}</b>
+              </Typography>{' '}
+            </Grid>{' '}
+            <Grid item xs={1}>
+              {' '}
+              <Select
+                value={client_info.type}
+                label={words_he['type']}
+                onChange={(e) => setClientInfo({ ...client_info, type: e.value })}
+                style={{ borderStyle: 'solid', borderWidth: 1, borderRadius: 5, width: 180 }}
+              >
+                <MenuItem value={words_he['private']}>{words_he['private']}</MenuItem>
+                <MenuItem value={words_he['company']}>{words_he['company']}</MenuItem>
+                <MenuItem value={words_he['department']}>{words_he['department']}</MenuItem>
+              </Select>{' '}
+            </Grid>{' '}
+          </Grid>
+        </Grid>
+
+        <Grid item xs={12}>
+          <Grid container justifyContent='center'>
+            {' '}
+            <Grid item xs={1}>
+              {' '}
+              <Typography>
+                <b>{words_he['phone']}</b>
+              </Typography>{' '}
+            </Grid>{' '}
+            <Grid item xs={1}>
+              {' '}
+              <InputBase
+                type='tel'
+                onChange={(e) => setClientInfo({ ...client_info, phone: e.target.value })}
+                defaultValue={client_info.phone}
+                style={{ borderStyle: 'solid', borderWidth: 1, borderRadius: 5, width: 180 }}
               />{' '}
-              {words_he['company']}
-            </li>
-            <li>
-              <input
-                type='radio'
-                value={words_he['department']}
-                name={'type'}
-                checked={client_info.type === words_he['department']}
-                onChange={(e) => setClientInfo({ ...client_info, type: e.target.value })}
+            </Grid>{' '}
+          </Grid>
+        </Grid>
+
+        <Grid item xs={12}>
+          <Grid container justifyContent='center'>
+            {' '}
+            <Grid item xs={1}>
+              {' '}
+              <Typography>
+                <b>{words_he['email']}</b>
+              </Typography>{' '}
+            </Grid>{' '}
+            <Grid item xs={1}>
+              {' '}
+              <InputBase
+                type='email'
+                onChange={(e) => setClientInfo({ ...client_info, email: e.target.value })}
+                defaultValue={client_info.email}
+                style={{ borderStyle: 'solid', borderWidth: 1, borderRadius: 5, width: 180 }}
               />{' '}
-              {words_he['department']}
-            </li>
-          </ul> }
-      //   </label>
-      //   <label>
-      //     {words_he['phone']}
-      //     <input type='tel' onChange={(e) => setClientInfo({ ...client_info, phone: e.target.value })} defaultValue={client_info.phone} />
-      //   </label>
-      //   <label>
-      //     {words_he['email']}
-      //     <input type='email' onChange={(e) => setClientInfo({ ...client_info, email: e.target.value })} defaultValue={client_info.email} />
-      //   </label>
-      //   <div>
-      //     <button type='button' className='btn btn-success m-2' onClick={handle_save} disabled={!enable_send}>
-      //       {words_he['save']}
-      //     </button>
-      //     <button type='button' className='btn btn-danger m-2' onClick={handle_delete}>
-      //       {words_he['delete']}
-      //     </button>
-      //   </div>
-      // </form> */}
-      <table>
-        <thead>
-          <tr>
-            <th scope='col' className='border-0 text-uppercase font-medium pl-4'>
-              {words_he['name']}
-            </th>
-            <th scope='col' className='border-0 text-uppercase font-medium pl-4'>
-              {words_he['type']}
-            </th>
-            <th scope='col' className='border-0 text-uppercase font-medium pl-4'>
-              {words_he['phone']}
-            </th>
-            <th scope='col' className='border-0 text-uppercase font-medium pl-4'>
-              {words_he['email']}
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>
-              <input type='text' onChange={(e) => setClientInfo({ ...client_info, name: e.target.value })} defaultValue={client_info.name} />
-            </td>
-            <td>
-              <ul>
-                <li>
-                  <input
-                    type='radio'
-                    value={words_he['private']}
-                    name={'type'}
-                    checked={client_info.type === words_he['private']}
-                    onChange={(e) => setClientInfo({ ...client_info, type: e.target.value })}
-                  />{' '}
-                  {words_he['private']}
-                </li>
-                <li>
-                  <input
-                    type='radio'
-                    value={words_he['company']}
-                    name={'type'}
-                    checked={client_info.type === words_he['company']}
-                    onChange={(e) => setClientInfo({ ...client_info, type: e.target.value })}
-                  />{' '}
-                  {words_he['company']}
-                </li>
-                <li>
-                  <input
-                    type='radio'
-                    value={words_he['department']}
-                    name={'type'}
-                    checked={client_info.type === words_he['department']}
-                    onChange={(e) => setClientInfo({ ...client_info, type: e.target.value })}
-                  />{' '}
-                  {words_he['department']}
-                </li>
-              </ul>
-            </td>
-            <td>
-              <input type='tel' onChange={(e) => setClientInfo({ ...client_info, phone: e.target.value })} defaultValue={client_info.phone} />
-            </td>
-            <td>
-              <input type='email' onChange={(e) => setClientInfo({ ...client_info, email: e.target.value })} defaultValue={client_info.email} />
-            </td>
-            <td></td>
-          </tr>
-        </tbody>
-      </table>
-      <button type='button' className='btn btn-success m-2' onClick={handle_save} disabled={!enable_send}>
-        {words_he['save']}
-      </button>
-      <button type='button' className='btn btn-danger m-2' onClick={handle_delete}>
-        {words_he['delete']}
-      </button>{' '}
+            </Grid>{' '}
+          </Grid>
+        </Grid>
+
+        <Grid item xs={12}>
+          <Grid container justifyContent='center' >
+            {' '}
+            <Grid item xs={1}>
+              {' '}
+              <button type='button' className='btn btn-success m-2' onClick={handle_save} disabled={!enable_send}>
+                {words_he['save']}
+              </button>{' '}
+            </Grid>{' '}
+            <Grid item xs={1}>
+              {' '}
+              <button type='button' className='btn btn-danger m-2' onClick={handle_delete}>
+                {words_he['delete']}
+              </button>{' '}
+            </Grid>{' '}
+          </Grid>
+        </Grid>
+      </Grid>
     </div>
   )
 }
